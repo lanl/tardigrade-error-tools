5e0dd8edcda1ccff06fae81e344f4c8429b736a7
Kyle Andrew Brindley <kbrindley@lanl.gov>
2020-06-01 09:10:25 -0600

    FEAT: add rudimentary attempt to find cmake 3 executable in env

--- new_build.sh
+++ new_build.sh
@@ -1,5 +1,12 @@
+set -Eeuxo pipefail
+
+cmake_exec=$(which cmake3)
+if [[ $? -ne 0 ]]; then
+    cmake_exec=$(which cmake)
+fi
+
 rm -r build/
 mkdir build
 cd build
-cmake ..
-cmake --build .
+${cmake_exec} ..
+${cmake_exec} --build .
