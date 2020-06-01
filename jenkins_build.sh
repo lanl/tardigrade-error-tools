a8201e688782847ae06bb7732b76fccf6da8fa0c
Kyle Brindley <kbrindley@lanl.gov>
2020-06-01 09:52:27 -0600

    FEAT: use cmake during jenkins CI script

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -4,21 +4,30 @@
 source /apps/intel2016/bin/ifortvars.sh -arch intel64 -platform linux
 
 # Make bash script more like high-level languages.
-# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
-# Have to do this after sourcing ifortvars.sh becuase the shell script has unbound variables
 set -Eeuxo pipefail
 
 # Clone and update dependencies
 source update_dependencies.sh
 
-# Build repo tests
-cd ${workdir}/src/cpp/tests/${repo}/
-if [ -f ${repo}.o ]; then
-    make clean
+# Find cmake3 executable
+if [ -x "$(command -v cmake3)" ]; then
+    cmake_exec=$(command -v cmake3)
+elif [ -x "$(command -v cmake)" ]; then
+    cmake_exec=$(command -v cmake)
+else
+    echo "Could not find cmake executable"
+    exit 1
 fi
-make
+
+# Clean and build repo tests
+rm -rf build/
+mkdir build
+cd build
+${cmake_exec} ..
+${cmake_exec} --build ${tests}
 
 # Perform repo tests
+cd "${tests}"
 ./test_${repo}
 
 # Check for failed tests
