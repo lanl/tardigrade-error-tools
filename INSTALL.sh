0fd8b41ab278bf6070c216632d713ab8f7fa230e
Kyle Brindley <kbrindley@lanl.gov>
2021-05-11 10:48:34 -0600

    FEAT: install entire project in CD script

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,4 +2,4 @@
 set -Eeuxo pipefail
 cd "build"
 conda_env_path=$(conda info | grep "active env location" | cut -f 2 -d :)
-cmake --install src/cpp --prefix ${conda_env_path}
+cmake --install . --prefix ${conda_env_path}
